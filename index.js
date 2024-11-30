72f0d72958e5d9e79257c0040d2275d8c58048de
msharif123 <mohamed.sharif.abdiwahab>
2024-12-01 00:32:23 +0100

    add some code and port number into the index code

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require ("express")
+ const app = express()
+
+ app.get ("/", (req, res)=>{
+     res.send("Hello world")
+ })
+ 
+const port  = 3000
+
+
+
+ app.listen(port,()=>{
+     console.log("This serveris runing in locallhost port 3000")
+ } )

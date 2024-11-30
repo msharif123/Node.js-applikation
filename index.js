7f62e33e8ecebf07460bd160c14d7378104e341d
msharif123 <mohamed.sharif.abdiwahab>
2024-12-01 00:56:42 +0100

    install package-json

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require ("express")
- const app = express()
+// const express = require ("express")
+//  const app = express()
 
- app.get ("/", (req, res)=>{
-     res.send("Hello world")
- })
+//  app.get ("/", (req, res)=>{
+//      res.send("Hello world")
+//  })
  
-const port  = 3000
+//  const port  = 3000
 
 
 
- app.listen(port,()=>{
-     console.log("This serveris runing in locallhost port 3000")
- } )
+//  app.listen(port,()=>{
+//      console.log("This serveris runing in locallhost port 3000")
+//  
+
+

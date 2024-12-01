cdd608f5eeb2cd6bed0e027158b95dfbbbc57005
msharif123 <mohamed.sharif.abdiwahab>
2024-12-01 01:43:38 +0100

    installera express package

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-// const express = require ("express")
-//  const app = express()
+const express = require ("express")
+ const app = express()
 
-//  app.get ("/", (req, res)=>{
-//      res.send("Hello world")
-//  })
+ app.get ("/", (req, res)=>{
+     res.send("Hello world my name is sharif  ")
+ })
  
-//  const port  = 3000
+ const port  = 3000
 
 
 
-//  app.listen(port,()=>{
-//      console.log("This serveris runing in locallhost port 3000")
-//  
-
-
+ app.listen(port,()=>{
+     console.log("This serveris runing in locallhost port 3000")
+})

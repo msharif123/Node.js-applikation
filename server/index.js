aa76728163691d5359345b31342863fa54f6d28d
msharif123 <mohamed.sharif.abdiwahab>
2024-12-03 15:23:13 +0100

    modified server , and add some code

--- server/index.js
+++ server/index.js
@@ -1,37 +1,119 @@
-const express = require ("express")
+ const express = require ("express")
+const app = express()
 
- const path= require ("path")
- const app = express()
- require("dotenv").config()
+ const info= require ("../modules/info")
 
- app.use(express.json())
+//env
+require ("dotenv").config()
+
+ app.use(express.json()) 
 
 
- const port = process.env.port || 3770
+ const port = process.env.PORT || 3500  // belastnng - loadig 
 
  const connectDB= require ("../Database/db")
+const Info = require("../modules/info")
+
+
 
  app.get ("/", (req, res)=>{
      res.send("Hello world")
  })
 
+
+
+ app.get ("/info", async (req, res)=>{
+
+   try {
+    const info = await Info.find({})
+    res.status(200).json(info)
+
+    
+   } catch (error) {
+    res.status(500).json({message:error.message})
+    
+   }
+ })
+//  
+
+ app.get ("/info/:id", async (req,res)=>{
+    try {
+        const {id}= req.params
+        const info = await Info.findById(id)
+        res.status(200).json(info)
+      
+      
+
+    } catch (error) {
+        res.status(500).json({message:error.message})
+        
+    }
+ })
+ app.post("/info", async (req, res)=>{
+   // console.log(req.body)  
+ //res.send(req.body)      // console log to try if it works with postman {"":"",}
+   
+try {
+    const newInfo= await info.create(req.body)
+    res.status(200).json(newInfo)
+    
+} catch (error) {
+    console.log(error.message)
+    res.status(500).json({message: error.message})
+    
+}
+    
+
+ })
+ //update 
+
+ app.put("/info/:id", async( req, res)=>{
+    try {
+        const {id} = req.params
+        const info = await Info.findByIdAndUpdate(id, req.body)
+        res.status(200).json(info)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+ })
+
+
+//delete 
+
+app.delete("/info/:id", async (req, res)=>{
+  try{  
+     const {id} = req.params
+    const info = await Info.findByIdAndDelete(id)
+    res.status(200).json(info)
+    
+
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+/
+
  
-// app.get("/", (req,res)=>{
-//     res.sendFile(path.join(__dirname, "./html/index.html"))
-// })
+ app.get("/", (req,res)=>{
+     res.sendFile(path.join(__dirname, "./html/index.html"))
+ })
 
  
 
 
 connectDB()
 
-
+ 
 
 
 //  app.listen(port,()=>{
 //      console.log("This serveris runing in locallhost port 3700")
 // })
-
+ 
 
 
 
@@ -41,5 +123,5 @@ connectDB()
 
 
 app.listen( port, ()=>{
-    console.log (`This server is runing on port ${port}`)
+console.log (`This server is runign on port ${port}`)
 })
